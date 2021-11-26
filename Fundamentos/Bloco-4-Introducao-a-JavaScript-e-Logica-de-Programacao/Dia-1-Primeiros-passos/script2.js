e598cf602c98189a94de058300a8f0383c999314
Miguel <miguellofredo@gmail.com>
2021-11-26 18:40:54 -0300

    2 script

---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-1-Primeiros-passos/script2.js
@@ -0,0 +1,29 @@
+let patientId = 50;
+patientId= '50'
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof(patientId));
+console.log(typeof(isEnrolled));
+console.log(typeof(patientInfo));
+console.log(typeof(patientEmail));
+console.log(typeof(patientAge));
+
+// Vamos fazer algumas operações simples para encontrarmos a área e o perímetro de um retângulo de base 5 e altura 8.
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+
+const perimeter = base * 2 + height * 2;
+console.log(perimeter);
+
+
+
---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-1-Primeiros-passos/scripts.js
@@ -0,0 +1,25 @@
+const myName = "Miguel";
+const birthCity = "Buenos Aires";
+let birthYear = 1985;
+birthYear = 2030;
+birthCity ="Bahia";
+
+console.log(myName);
+console.log(birthYear);
+console.log(birthCity);
+console.log(birthYear);
+console.log(birthCity);
+
+
+/*
+Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+
+Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+
+Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+
+Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+
+Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔 */
