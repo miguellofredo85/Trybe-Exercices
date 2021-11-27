14cf78a02b16edb19cb2d9f138d419393768a7da
Miguel <miguellofredo@gmail.com>
2021-11-27 10:20:51 -0300

    exercicio 4

--- Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-1-Primeiros-passos/exercicio.js
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-1-Primeiros-passos/exercicio.js
@@ -42,6 +42,18 @@ else{
 
 
 // 4 Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+const valorDefinido = 44;
+
+if(valorDefinido > 0){
+    console.log("positive")
+}
+else if(valorDefinido < 0){
+    console.log("negative")
+}
+else{
+    console.log("zero")
+}
+
 
 
 // 5 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.Um ângulo será considerado inválido se não tiver um valor positivo.
