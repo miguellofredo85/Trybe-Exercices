2eb64e78e0536c8bbcd0544f3a5680779555de8d
Miguel <miguellofredo@gmail.com>
2021-11-29 11:13:11 -0300

    messae

---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-2-Array-e-loop-for/forOf.js
@@ -0,0 +1,29 @@
+/*Exercício 1
+Utilize o for/of para imprimir os elementos da lista names com o console.log() :*/
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for(let cadaNome of names){
+    console.log(cadaNome);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
