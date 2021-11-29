56c67d683749efd2b158df152648afda3ee13c43
Miguel <miguellofredo@gmail.com>
2021-11-29 11:06:56 -0300

    ex

---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-2-Array-e-loop-for/arrays.js
@@ -0,0 +1,33 @@
+//Exercício 1
+//Obtenha o valor "Serviços" do array menu :
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1] ;
+
+//console.log(menuServices);
+
+//Exercício 2
+//Procure o índice do valor "Portfólio" do array menu :
+
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+
+Exercício 3
+Adicione o valor "Contato" no final do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
+*/
+
+
+
+
+
+
+
+
+
---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-2-Array-e-loop-for/forLoop.js
@@ -0,0 +1,30 @@
+//Exercício 1
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let i = 0; i < groceryList.length; i+=1){
+    console.log(groceryList[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
