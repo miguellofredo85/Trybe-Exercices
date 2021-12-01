3e8e44ea5dfe5b88a77b23d5e706109cb7bee685
Miguel <miguellofredo@gmail.com>
2021-11-30 23:53:23 -0300

    bonus 1

---
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-3-Logica-de-Programacao-e-Algoritmos/exercises.js
@@ -0,0 +1,59 @@
+/*let valorN = 5; 
+let contenOne = '*';
+let espaco = '';
+let newposition = valorN;
+
+for(let i = 0; i<valorN; i+=1){
+        for(let col = 0; col<=valorN; col+=1){
+        if(col<newposition){
+                espaco=espaco+' ';
+        }else{
+                espaco=espaco+contenOne;
+        }     
+}
+console.log(espaco);
+newposition-=1;  
+espaco = '';
+}
+*/
+/*
+let n = 5;
+let asterisco = '*';
+let aspas = '';
+
+for(let i=0; i<n; i+=1){
+        aspas=aspas+asterisco;
+};
+for(let j=0; j<n; j+=1){
+                console.log(aspas);
+        }*/
+
+
+        /*let n = 5;
+        let asterisco = '*';
+        let aspas = '';
+       
+        for(let i=0; i<n; i+=1){
+         aspas=aspas+asterisco;
+        console.log(aspas)
+                }*/
+/*let base = 5;
+let estrelha = '*';
+let espaco = '';
+let medioBase = (base+1)/2;
+let izquerda = medioBase;
+let direita = medioBase;
+
+for(let linha=0; linha<= medioBase; linha+=1){
+        for(let coluna=0; coluna<=base; coluna+=1){
+                if(coluna>izquerda && coluna<direita){
+                        espaco=espaco+estrelha;
+                }else{
+                        espaco=espaco+' ';
+                };
+        }
+console.log(espaco);
+espaco='';
+izquerda-=1;
+direita+=1;
+}*/
