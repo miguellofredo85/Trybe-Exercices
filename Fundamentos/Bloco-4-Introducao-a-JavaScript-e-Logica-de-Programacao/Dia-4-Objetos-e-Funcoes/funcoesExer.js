a084d2ccc737d2702019a69c087296181d2a7223
Miguel <miguellofredo@gmail.com>
2021-12-03 18:10:18 -0300

    sdgsdg

--- Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-4-Objetos-e-Funcoes/funcoesExer.js
+++ Fundamentos/Bloco-4-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-4-Objetos-e-Funcoes/funcoesExer.js
@@ -1,5 +1,6 @@
-/*1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
-function checkarPalindromo(palindromo){
+//1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+
+/*function checkarPalindromo(palindromo){
   for(let i in palindromo){
       if(palindromo[i] !== palindromo[(palindromo.length-1)-i])
       {
@@ -11,10 +12,11 @@ function checkarPalindromo(palindromo){
   }
 }
 
+
 console.log(checkarPalindromo('arara'));
 console.log(checkarPalindromo('desenvolvimento'));*/
-/*2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
-function enteros(array){
+// - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+/*function enteros(array){
     let valorMaior=0;
     for(let index in array){
         if(array[valorMaior]<array[index]){
@@ -39,8 +41,8 @@ console.log(enteros([2, 4, 6, 7, 10, 0, -3]));*/
 
 //4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 /*
-function funcionPrueba(array){
-    
+function funcionProba(array){
+   
     for(let i in array){
         if(array[0].length<array[i].length){
             array[0]=array[i]
@@ -48,25 +50,31 @@ function funcionPrueba(array){
     }
     return array[0];
 }
-console.log(funcionPrueba(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
+console.log(funcionProba(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 */
 
 //5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete. Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
 
-function enteroRepeat(array){
-    
-    for(let i in array){
-        let numeroUm=array[i];
-        for(let j in array){
-            if(numeroUm===array[j]){
-                
-            }
-
+let a = [2, 3, 2, 5, 8, 2, 3]
+let num = []
+for (let i in a){
+  let cont = 0
+  for (let j = 0; j<a.length; j+=1){
+    if(a[i] == a[j]){
+    cont = cont+1
+  }
+  }
+  num.push(cont)
+}
+function enteros(array){
+    let valorMaior=0;
+    for(let index in array){
+        if(valorMaior<array[index]){
+            valorMaior=array[index]
         }
     }
-
+    return a[num.indexOf(valorMaior)]
 }
-
-
-
+let array = num
+console.log(enteros(a))
 
