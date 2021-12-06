a9f2a4b49bd0aa112a6cd9949f41edc4df59b53a
Miguel <miguellofredo@gmail.com>
2021-12-06 15:43:01 -0300

    exercicios array loop remove childs elements

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/Elementos/script.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/Elementos/script.js
@@ -1,3 +1,4 @@
+/*
 let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
 let pai = elementoOndeVoceEsta.parentElement;
@@ -9,14 +10,14 @@ primeiroFilhoDoFilho.innerText = 'Agregando algum texto';
 
 let primeiroFilho = pai.firstElementChild;
 
-elementoOndeVoceEsta.previousElementSibling;
+let primeiroFilho2 = elementoOndeVoceEsta.previousElementSibling;
 
 let atencao = elementoOndeVoceEsta.nextSibling;
 
 let tercerFilho = elementoOndeVoceEsta.nextElementSibling;
 
 let tercerFilho2 = pai.lastElementChild.previousElementSibling; 
-
+*/
 
 /*
 Acesse o elemento elementoOndeVoceEsta .
@@ -27,10 +28,44 @@ Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
 Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
 Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
 Agora acesse o terceiroFilho a partir de pai .
+
+CRIANDO ELEMENTOS
+
+Crie um irmão para elementoOndeVoceEsta .
+Crie um filho para elementoOndeVoceEsta .
+Crie um filho para primeiroFilhoDoFilho .
+A partir desse filho criado, acesse terceiroFilho .
+
 */
+/*
+const pai1 = document.getElementById('pai');
+const irmaoEOVE = document.createElement('section');
+irmaoEOVE.id = 'irmaoEOVE';
+pai.appendChild(irmaoEOVE);
 
 
+const filhoEOVE = document.createElement('section');
+filhoEOVE.id('filhoEOVE');
+elementoOndeVoceEsta.appendChild(filhoEOVE);
 
+let filhoPFDF = document.createElement('p');
+filhoPFDF.id('filhoPFDF');
+primeiroFilhoDoFilho.appendChild(filhoPFDF);
 
+let terceiroFilho = filhoPFDF.parentElement.parentElement.nextElementSibling;   
+
+*/
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho .
+
+const pai = document.querySelector('#pai');
+
+for(let index = pai.childNodes.length-1; index > 0; index -= 1){
+    let posicion = pai.childNodes[index];
+    if (posicion !== 'elementoOndeVoceEsta'){
+        posicion.remove();
+    }
+}
+let segundoFdF = document.querySelector('#segundoEUltimoFilhoDoFilho');
+segundoFdF.remove();
 
 
