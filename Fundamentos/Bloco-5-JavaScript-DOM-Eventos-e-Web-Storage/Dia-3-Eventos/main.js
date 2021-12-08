df60c5886683c4ddc5999810423d033e83fef34d
Miguel <miguellofredo@gmail.com>
2021-12-07 22:41:02 -0300

    msdfp

--- Fundamentos/Bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/Dia-3-Eventos/main.js
+++ Fundamentos/Bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/Dia-3-Eventos/main.js
@@ -26,14 +26,22 @@ thirdLi.addEventListener('click', addTech)
 input.addEventListener('input', function (event){
     const caixas = document.querySelector('.tech');
     caixas.innerText = event.target.value;
-}
+})
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+const meuTop = document.querySelector('#my-spotrybefy');
+meuTop.addEventListener('dblclick', function(){
+        window.location.replace('www.google.com');
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+meuTop.addEventListener('mouseover', function(event){
+    event.target.style.color = 'yellow';
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 
