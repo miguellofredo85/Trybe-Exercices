79a2ed75dbef522a6825fc0de82e78d492749e50
Miguel-Angel-Lofredo <miguellofredo@gmail.com>
2022-01-02 20:47:27 -0300

    ex 6.1

---
+++ Fundamentos/Bloco-6-HTML-e-CSS-Forms-Flaxbox-e-Responsivo/exercices6-1/script.js
@@ -0,0 +1,15 @@
+const select = document.querySelector('#select-estados');
+let cpf = document.querySelector('#cpf');
+
+
+// let cpfValidate = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
+
+function createStates(){
+    let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
+    for(let i of estados){
+        let opcao = document.createElement('option');
+        opcao.innerText = i;
+        select.append(opcao);
+    }
+}
+createStates()
