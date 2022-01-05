a51f0ae854623220870db254ff6983a2091c41d9
Miguel-Angel-Lofredo <miguellofredo@gmail.com>
2022-01-05 00:56:27 -0300

    ex 6-1-e-6-2

---
+++ Fundamentos/Bloco-6-HTML-e-CSS-Forms-Flaxbox-e-Responsivo/Dia-1-HTML-e-CSS-Forms/exercices6-1/script.js
@@ -0,0 +1,175 @@
+const select = document.querySelector('#select-estados');
+const cpf = document.querySelector('#cpf');
+const form = document.querySelector('#formulario');
+const btnEnviar = document.querySelector('#enviar');
+const btnLimpar = document.querySelector('#limpar')
+const inputs = document.querySelectorAll('input');
+const text = document.querySelectorAll('textarea')
+const container = document.querySelector('#container');
+
+function createStates(){
+    let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
+    for(let i of estados){
+        let opcao = document.createElement('option');
+        opcao.innerText = i;
+        select.appendChild(opcao);
+        opcao.setAttribute('value', i)
+    }
+}
+createStates()
+
+function nome(){
+    let name = document.querySelector('#name');
+    if(name.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'nome'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Nome: ${name.value}`
+    }
+}
+
+function email(){
+    let email = document.querySelector('#email');
+    if(email.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'email'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Email: ${email.value}`
+    }
+}
+
+function endereco(){
+    let endereco = document.querySelector('#endereco');
+    if(endereco.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'endereco'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Endereco: ${endereco.value}`
+    }
+}
+
+function cpfs(){
+    let cpf = document.querySelector('#cpf');
+    if(cpf.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'cpf'
+        container.appendChild(paragraph);
+        paragraph.innerText = `CPF: ${cpf.value}`
+    }
+}
+
+function cidade(){
+    let cidade = document.querySelector('#cidade');
+    if(cidade.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'cidade'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Cidade: ${cidade.value}`
+    }
+}
+
+function estado(){
+    if(select.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'select'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Estado: ${select.value}`
+    }
+}
+
+function casa(){
+    let casa = document.querySelector('#casa');
+    if(casa.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'casa'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Mora em ${casa.value}`
+    }
+}
+
+function apartamento(){
+    let apartamento = document.querySelector('#apartamento');
+    if(apartamento.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'apartamento'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Mora em ${apartamento.value}`
+    }
+}
+
+function trabalho(){
+    let trabalho = document.querySelector('#trabalho');
+    if(trabalho.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'trabalho'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Resumo do curriculo : ${trabalho.value}`
+    }
+}
+
+function cargo(){
+    let cargo = document.querySelector('#cargo');
+    if(cargo.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'cargo'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Cargo : ${cargo.value}`
+    }
+}
+
+function descricao(){
+    let descricao = document.querySelector('#descricao');
+    if(descricao.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'descricao'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Descricao de cargo : ${descricao.value}`
+    }
+}
+
+function data(){
+    let data = document.querySelector('#data');
+    if(data.value){
+        let paragraph = document.createElement('p');
+        paragraph.className = 'data'
+        container.appendChild(paragraph);
+        paragraph.innerText = `Data de Inicio no cargo : ${data.value}`
+    }
+}
+
+function btnSend(){
+    nome();
+    email();
+    cpfs();
+    endereco();
+    btns();
+    cidade();
+    estado();
+    casa();
+    apartamento();
+    trabalho();
+    cargo();
+    descricao();
+    data();
+}
+
+function stopDefault(event){
+    event.preventDefault();
+    btnSend();
+}
+
+btnEnviar.addEventListener('click', stopDefault);
+
+function btns(){
+    for (let i = 0; i < inputs.length; i +=1){
+        if(!inputs[i].value){
+        alert(`Preencher ${inputs[i].name}`)
+      }
+    }
+  }
+
+
+btnLimpar.addEventListener('click', function limparForm(){
+    inputs.value = "";
+    text.value = "";
+})
--- Fundamentos/Bloco-6-HTML-e-CSS-Forms-Flaxbox-e-Responsivo/Dia-2-bibliotecas-javascript-e-frameworks-css/exercicios-6-2/script.js
+++ Fundamentos/Bloco-6-HTML-e-CSS-Forms-Flaxbox-e-Responsivo/Dia-2-bibliotecas-javascript-e-frameworks-css/exercicios-6-2/script.js
@@ -6,14 +6,16 @@ const btnLimpar = document.querySelector('#limpar')
 const inputs = document.querySelectorAll('input');
 const text = document.querySelectorAll('textarea')
 
-// let cpfValidate = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/;
+
+
+document.getElementById('data').DatePickerX.init();
 
 function createStates(){
     let estados = ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MS','MT','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
     for(let i of estados){
         let opcao = document.createElement('option');
         opcao.innerText = i;
-        select.append(opcao);
+        select.appendChild(opcao);
         opcao.setAttribute('value', i)
     }
 }
@@ -21,18 +23,16 @@ createStates()
 
 function stopDefault(event){
     event.preventDefault()
-    btns()
+    btns();
 }
+
 btnEnviar.addEventListener('click', stopDefault);
 
-function btns(){
-    for (let i = 0; i < inputs.length; i +=1){
-        if(!inputs[i].value){
-        alert(`Preencher ${inputs[i].name}`)
-      }
-    }
-  }
 btnLimpar.addEventListener('click', function limparForm(){
     inputs.value = "";
     text.value = "";
 })
+
+
+
+
