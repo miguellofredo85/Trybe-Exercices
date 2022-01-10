16b0dd82c73e9f7c90324ff1e40554a3047f7e59
Miguel-Angel-Lofredo <miguellofredo@gmail.com>
2022-01-10 08:33:08 -0300

    refact

--- Fundamentos/Bloco-7-Introducao-a-JS-ES6-e-Testes-Unitarios/Dia-1-JS-ES6-let-const-arrow-function-e-template-literals/ponto 4/script.js
+++ Fundamentos/Bloco-7-Introducao-a-JS-ES6-e-Testes-Unitarios/Dia-1-JS-ES6-let-const-arrow-function-e-template-literals/ponto 4/script.js
@@ -1,3 +1,11 @@
+const abilities = ['terminal linux', ' git', ' HTML', ' CSS', ' JS'];
+let twoPhrases = word => {
+let phraseOne = `Tryber ${word} aqui!`;
+let newArr = abilities.sort().toString()
+return `${phraseOne} Minhas cinco habilidades sao: ${newArr}, #goTrybe`
+    }
+console.log(twoPhrases('Miguel'))
+
 // const abilities = [' terminal linux', ' git', ' HTML', ' CSS', ' JS'];
 
 // let phraseOne = word => `Tryber ${word} aqui!`;
@@ -7,12 +15,3 @@
 //     return `${phraseOne('Miguel')} Minhas cinco habilidades sao: ${newArr}`
 //     }
 // console.log(phraseTwo())
-
-const abilities = ['terminal linux', ' git', ' HTML', ' CSS', ' JS'];
-function twoPhrases(word) {
-let phraseOne = `Tryber ${word} aqui!`;
-let newArr = abilities.sort().toString()
-return `${phraseOne} Minhas cinco habilidades sao: ${newArr}, #goTrybe`
-    }
-console.log(twoPhrases('Miguel'))
-
