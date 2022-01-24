9c42f67ac7083631ac27bd4c3ba9b8dc395329f4
Miguel-Angel-Lofredo <miguellofredo@gmail.com>
2022-01-23 22:34:28 -0300

    fazendo exercises reduce

--- Fundamentos/Bloco-8-HOF-EM6/Dia-4-HOF-reduce/exercise5.js
+++ Fundamentos/Bloco-8-HOF-EM6/Dia-4-HOF-reduce/exercise5.js
@@ -8,7 +8,9 @@ const names = [
   
   function containsA() {
     // escreva seu código aqui
+    let array = names.reduce((acc, val) => acc + val).split('')
+    return array.filter((elefilter) => (elefilter==='A' || elefilter==='a')).length
   }
-
+console.log(containsA())
   //5 - Dada o array de nomes, retorne a quantidade de vezes em que 
   //aparecem a letra a maiúscula ou minúscula.
