81445d64b8e99dfb59d48800440d0d9777b446e0
Miguel-Angel-Lofredo <miguellofredo@gmail.com>
2022-01-23 23:03:33 -0300

    fazendo exercises reduce

--- Fundamentos/Bloco-8-HOF-EM6/Dia-4-HOF-reduce/exercise6.js
+++ Fundamentos/Bloco-8-HOF-EM6/Dia-4-HOF-reduce/exercise6.js
@@ -8,14 +8,19 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((elemento, i) =>(
+    {
+      name: elemento,
+      average: grades[i].reduce((acc, ele) => acc + ele)/grades[i].length
+    }
+  ))
 }
-
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
+console.log(studentAverage());
+// const expected = [
+//   { name: 'Pedro Henrique', average: 7.8 },
+//   { name: 'Miguel', average: 9.2 },
+//   { name: 'Maria Clara', average: 8.8 },
+// ];
 
 // 6. - Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato 
 // { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notas refere-se ao aluno 
